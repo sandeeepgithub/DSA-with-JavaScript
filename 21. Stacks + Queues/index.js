0332da22121802e70f8326c93fb006a6af104e3d
Sandeep <sandeeepgithub@gmail.com>
2024-04-07 19:48:24 +0530

    21.1 - Stacks

---
+++ 21. Stacks + Queues/index.js
@@ -0,0 +1,64 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    push(val) {
+
+        let newNode = new Node(val)
+        
+        if(!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let temp = this.first
+            this.first = newNode
+            this.first.next = temp
+        }
+
+        return ++this.size
+    }
+
+    pop() {
+
+        if(this.size === 0) {
+            return null
+        }
+
+        let removalNode = this.first
+        
+        if(this.length === 1) {
+            this.first = null
+            this.last = null
+        } else {
+            this.first = removalNode.next
+        }
+
+        this.size--
+        return removalNode.val
+    }
+
+    multiplePush(val) {
+        let i = 0
+        while(i !== val) {
+            this.push(i)
+            i++
+        }
+        return this
+    }
+   
+}
+
+const list = new Stack("list")
+list.multiplePush(5)
+console.log(list)
+    
